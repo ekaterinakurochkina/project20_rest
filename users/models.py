808f2ab7d192573cebf138c527b4103238115e3c
Katerina Kurochkina <katifedr@mail.ru>
2025-04-13 11:22:28 +0500

    users model

--- users/models.py
+++ users/models.py
@@ -3,4 +3,26 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    pass
+    username = None
+    email = models.EmailField(unique=True, verbose_name="Email")
+    phone = models.CharField(max_length=35, verbose_name="Телефон", blank=True, null=True,
+                             help_text="Введите номер телефона")
+    avatar = models.ImageField(upload_to="users/avatars", verbose_name="Аватар", blank=True, null=True,
+                               help_text='Загрузите свой аватар')
+    is_active = models.BooleanField(default=True, verbose_name="Действующий")
+    token = models.CharField(max_length=100, verbose_name="Token", blank=True, null=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+        ordering = ['email']
+        permissions = [
+            ('can_inactivate', 'Can inactivate')
+        ]
+
