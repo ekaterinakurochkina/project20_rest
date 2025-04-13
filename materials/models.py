24baf2fba3a07b283d6063bfc551488cff2ac041
Katerina Kurochkina <katifedr@mail.ru>
2025-04-13 16:05:43 +0500

    CourseViewSet

--- materials/models.py
+++ materials/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+
+class Course(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Название курса", help_text="Введите название курса")
+    preview = models.ImageField(upload_to="materials/preview", blank=True, null=True, verbose_name="Картинка",
+                                help_text="Загрузите изображение")
+    description = models.TextField(blank=True, null=True, verbose_name="Описание курса",
+                                   help_text="Введите описание курса")
+
+    class Meta:
+        verbose_name = "Курс"
+        verbose_name_plural = "Курсы"
+
+
+class Lesson(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Название курса", help_text="Введите название курса")
+    course = models.ForeignKey(Course, on_delete=models.SET_NULL, verbose_name="Курс", help_text="Выберите курс",
+                               blank=True, null=True)
+    description = models.TextField(blank=True, null=True, verbose_name="Описание курса",
+                                   help_text="Введите описание курса")
+    preview = models.ImageField(upload_to="materials/preview", blank=True, null=True, verbose_name="Картинка",
+                                help_text="Загрузите изображение")
+    video_url = models.URLField(max_length=200, verbose_name="Ссылка на видео")
+
+    class Meta:
+        verbose_name = "Урок"
+        verbose_name_plural = "Уроки"
--- materials/views.py
+++ materials/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.viewsets import ModelViewSet
+from materials.models import Course, Lesson
+
+from materials.serializers import CourseSerializer, LessonSerializer
+
+
+class CourseViewSet(ModelViewSet):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
