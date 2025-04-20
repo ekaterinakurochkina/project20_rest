909d64a765206a3635c90f55ea8a1d8dca94c247
Katerina Kurochkina <katifedr@mail.ru>
2025-04-20 15:29:49 +0500

    Домашнее задание 30.2 фикстура уроков и курсов

--- users/models.py
+++ users/models.py
@@ -1,5 +1,9 @@
-from django.db import models
+from datetime import datetime
+
 from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+from materials.models import Course, Lesson
 
 
 class User(AbstractUser):
@@ -13,8 +17,7 @@ class User(AbstractUser):
                                help_text='Загрузите свой аватар')
 
     city = models.CharField(max_length=50, verbose_name="Город", blank=True, null=True,
-                             help_text="Введите название города")
-
+                            help_text="Введите название города")
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
@@ -26,5 +29,29 @@ class User(AbstractUser):
         verbose_name = 'Пользователь'
         verbose_name_plural = 'Пользователи'
         ordering = ['email']
+
+
+class Payments(models.Model):
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, verbose_name="Пользователь", help_text="Пользователь",
+                             blank=True, null=True, related_name='payments')
+    payment_date = models.DateTimeField(verbose_name="Дата оплаты", auto_now_add=True, help_text="Введите дату оплаты",
+                                        null=False, blank=True)
+    payment_course = models.ForeignKey(Course, on_delete=models.CASCADE, verbose_name="Оплаченный курс",
+                                       help_text="Оплаченный курс")
+    payment_lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE, verbose_name="Оплаченный урок",
+                                       help_text="Оплаченный урок")
+    payment_amount = models.PositiveIntegerField(verbose_name="Сумма оплаты", help_text="Сумма оплаты", null=False, blank=False)
+    PAYMENT_METHOD_CHOICES = [
+        ('cash', 'Наличные'),
+        ('transfer', 'Перевод'),
+    ]
+
+    payment_method = models.CharField(
+        max_length=15,
+        choices=PAYMENT_METHOD_CHOICES,
+        verbose_name="Метод оплаты",
+        help_text="Выберите метод оплаты: наличные или перевод.",
+        default='transfer'
+    )
 
 
