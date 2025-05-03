77289174fac4f54244a94d88c04e53603d154ba1
Katerina Kurochkina <katifedr@mail.ru>
2025-05-03 16:45:41 +0500

    31 разрешения для пользователей

--- materials/views.py
+++ materials/views.py
@@ -1,23 +1,44 @@
 from rest_framework.generics import CreateAPIView, ListAPIView, UpdateAPIView, DestroyAPIView, RetrieveAPIView
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 
 from materials.models import Course, Lesson
 from materials.serializers import CourseSerializer, LessonSerializer
+from users.permissions import IsModer, IsOwner
 
 
 class CourseViewSet(ModelViewSet):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
 
+    def get_permissions(self):
+        if self.action == "create":
+            self.permission_classes = (~IsModer,)
+        elif self.action in ["update", "retrieve"]:
+            self.permission_classes = (IsModer | IsOwner,)
+        elif self.action == "destroy":
+            self.permission_classes = (~IsModer | IsOwner,)
+        return super().get_permissions()
+
+    def perform_create(self, serializer):
+        course = serializer.save()
+        course.owner = self.request.user
+        course.save()
 
 class LessonCreateApiView(CreateAPIView):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
+    permission_classes = (~IsModer, IsAuthenticated)
 
+    def perform_create(self, serializer):
+        lesson = serializer.save()
+        lesson.owner = self.request.user
+        lesson.save()
 
 class LessonListApiView(ListAPIView):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
+    permission_classes = (IsAuthenticated)
 
     def get_queryset(self):
         queryset = super().get_queryset()
@@ -29,13 +50,15 @@ class LessonListApiView(ListAPIView):
 class LessonRetrieveApiView(RetrieveAPIView):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
+    permission_classes = (IsAuthenticated, IsModer | IsOwner)
 
 
 class LessonUpdateApiView(UpdateAPIView):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
-
+    permission_classes = (IsAuthenticated, IsModer | IsOwner)
 
 class LessonDestroyApiView(DestroyAPIView):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
+    permission_classes = (IsAuthenticated, ~IsModer, IsOwner)
--- users/views.py
+++ users/views.py
@@ -1,23 +1,16 @@
-import secrets
-from django_filters import filters
 from django_filters.rest_framework import DjangoFilterBackend
-from django.contrib.auth import logout
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.shortcuts import redirect, get_object_or_404
-from django.urls import reverse_lazy
-from django.views.generic import ListView
-from django.views.generic.edit import CreateView, DeleteView, UpdateView
+from rest_framework import filters
 from rest_framework import status
 from rest_framework.generics import CreateAPIView, ListAPIView, UpdateAPIView, DestroyAPIView, RetrieveAPIView
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
+
+from materials.models import Course, Lesson
 # from config.settings import EMAIL_HOST_USER
-from users.forms import UserRegisterForm, UserUpdateForm
 from users.models import Payments
 from users.models import User
-from materials.models import Course, Lesson
 from users.serializer import PaymentsSerializer, UserSerializer
-from rest_framework import filters
+
 
 class PaymentsCreateApiView(CreateAPIView):
     queryset = Payments.objects.all()
@@ -41,6 +34,7 @@ class PaymentsCreateApiView(CreateAPIView):
             return Response({'error': 'Необходимо указать курс или урок'}, status=status.HTTP_400_BAD_REQUEST)
 
         return Response(PaymentsSerializer(payment).data, status=status.HTTP_201_CREATED)
+
 
 class PaymentsListApiView(ListAPIView):
     queryset = Payments.objects.all()
@@ -72,96 +66,26 @@ class UserCreateApiView(CreateAPIView):
     permission_classes = (AllowAny,)  # открыли доступ к классу регистрации пользователя
 
     def perform_create(self, serializer):
-        user = serializer.save(is_active = True)
+        user = serializer.save(is_active=True)
         user.set_password(user.password)
         user.save()
 
 
-# class UserListView(LoginRequiredMixin, ListView):
-#     model = User
-#     template_name = "user_list.html"
-#     context_object_name = "users"
-#
-#     def get_context_data(self, **kwargs):
-#         context = super().get_context_data(**kwargs)
-#         # Получаем всех пользователей и добавляем информацию о принадлежности к группе
-#         for user in context['users']:
-#             user.is_manager = user.groups.filter(name="Менеджер").exists()
-#         return context
-#
-#
-# class UserCreateView(CreateView):
-#     model = User
-#     form_class = UserRegisterForm
-#     success_url = reverse_lazy("users:login")
-#
-#     def form_valid(self, form):
-#         user = form.save()
-#         user.is_active = True
-#         token = secrets.token_hex(16)  # генерируем токен
-#         user.token = token
-#         user.save()
-#         # host = self.request.get_host()  # получаем хост, откуда пришел пользователь
-#         # url = f'http://{host}/users/email-confirm/{token}/'
-#         # try:
-#         #     send_mail(
-#         #         subject="Подтверждение почты",
-#         #         message=f"""Спасибо, что зарегистрировались в нашем сервисе!
-#         #         Для подтверждения регистрации перейдите по ссылке {url}""",
-#         #         from_email=EMAIL_HOST_USER,
-#         #         recipient_list=[user.email]
-#         #     )
-#         # except Exception as e:
-#         #     print(f'Error sending email: {e}')
-#         # return super().form_valid(form)
-#
-#     # def get_context_data(self, **kwargs):
-#     #     context = super().get_context_data(**kwargs)
-#     #     context['is_manager'] = self.request.user.groups.filter(name="Менеджер").exists()
-#     #     return context
-#
-#
-# def email_verification(request, token):
-#     user = get_object_or_404(User, token=token)
-#     user.is_active = True
-#     user.save()
-#     return redirect(reverse_lazy("users:login"))
-#
-#
-# class UserUpdateView(LoginRequiredMixin, UpdateView):
-#     model = User
-#     form_class = UserUpdateForm
-#     template_name = "users:user_form.html"
-#     success_url = reverse_lazy("users:user_edit")
-#
-#     # def dispatch(self, request, *args, **kwargs):
-#     #     # Проверяем, является ли текущий пользователь суперпользователем
-#     #     if not request.user.is_superuser:
-#     #         raise PermissionDenied("У вас нет прав для редактирования этого пользователя.")
-#     #     return super().dispatch(request, *args, **kwargs)
-#     #
-#     # def get_object(self, queryset=None):
-#     #     # Загружаем объект, но проверка прав уже выполнена в dispatch
-#     #     return super().get_object(queryset)
-#
-#
-# class UserDeleteView(LoginRequiredMixin, DeleteView):
-#     model = User
-#     template_name = "user_confirm_delete.html"
-#     success_url = reverse_lazy("users:user_list")
-#
-#     # def get_object(self, queryset=None):
-#     #     self.object = super().get_object(queryset)
-#     #
-#     #     # Проверяем, является ли пользователь суперпользователем
-#     #     if self.request.user.is_superuser:
-#     #         return self.object  # Суперпользователь может удалять любого
-#     #
-#     #     # Проверяем, является ли пользователь менеджером
-#     #     if self.request.user.is_staff:  # Предполагаем, что менеджер имеет is_staff
-#     #         if self.object.is_superuser:
-#     #             raise PermissionDenied("Менеджер не может удалить суперпользователя.")
-#     #         return self.object  # Менеджер может удалять обычного пользователя
-#     #
-#     #     # Если пользователь не суперпользователь или менеджер
-#     #     raise PermissionDenied("У вас нет прав для удаления этого пользователя.")
+class UserUpdateApiView(UpdateAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+
+class UserListApiView(UpdateAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+
+class UserRetrieveApiView(RetrieveAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+
+class UserDestroyApiView(DestroyAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
