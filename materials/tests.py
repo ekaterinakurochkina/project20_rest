785cdc84785966d083450fdae6095b626e82780f
Katerina Kurochkina <katifedr@mail.ru>
2025-05-17 14:46:29 +0500

    32 тесты

--- materials/tests.py
+++ materials/tests.py
@@ -1,3 +1,4 @@
+from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
 
@@ -10,7 +11,6 @@ class MaterialsTestCase(APITestCase):
     def setUp(self):
         self.user = User.objects.create(email="katifedr@mail.ru")
         self.course = Course.objects.create(name='Test Course', owner=self.user)
-        # self.course = Course.objects.create(name="Математика")
         self.lesson = Lesson.objects.create(name="Устный счет", description="Счет для детей", owner=self.user,
                                             preview=None, video_url="https://www.youtube.com/testlesson/",
                                             course=self.course)
@@ -47,26 +47,48 @@ class MaterialsTestCase(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.json(),
                          {'count': 2, 'next': None, 'previous': None, 'results': [
-                             {'id': 3, 'name': 'Устный счет', 'description': 'Счет для детей', 'preview': None,
+                             {'id': 6, 'name': 'Устный счет', 'description': 'Счет для детей', 'preview': None,
                               'video_url': 'https://www.youtube.com/testlesson/', 'course': 'Test Course'},
-                             {'id': 4, 'name': 'Test list', 'description': 'Test list', 'preview': None,
+                             {'id': 7, 'name': 'Test list', 'description': 'Test list', 'preview': None,
                               'video_url': '', 'course': None}]}
                          )
 
     def test_lesson_retrieve(self):
         """Тестирование просмотра урока"""
-        response = self.client.get("/lessons/2/", format="json")
+        response = self.client.get("/lessons/3/", format="json")
         print(response.json())
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.json(),
-                         {'id': 4, 'name': 'Устный счет', 'description': 'Счет для детей', 'preview': None,
-                          'video_url': 'https://www.youtube.com/testlesson/', 'course': None}
+                         {'id': 3, 'name': 'Устный счет', 'description': 'Счет для детей', 'course': 'Test Course',
+                          'description': 'Счет для детей', 'id': 3, 'name': 'Устный счет', 'preview': None,
+                          'video_url': 'https://www.youtube.com/testlesson/'}
                          )
         self.assertTrue(Lesson.objects.all().exists())
 
-    # def test_lesson_update(self):
-        """Тестирование вывода списка уроков"""
+    def test_lesson_update(self):
+        """Тестирование обновления урока"""
+        update_data = {
+            'id': 6,
+            'name': 'Устный счет (обновленный)',
+            'description': 'Обновленное описание для устного счета',
+            'preview': None,
+            'video_url': 'https://www.youtube.com/testlesson/updated',
+            'course': self.course.id,
+        }
+
+        response = self.client.put("/lessons/6/update/", data=update_data, format="json")
+
+        print(response.json())
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.lesson.refresh_from_db()  # Обновляем объект из базы данных
+        self.assertEqual(self.lesson.name, 'Устный счет (обновленный)')
+        self.assertEqual(self.lesson.description, 'Обновленное описание для устного счета')
+        self.assertEqual(self.lesson.video_url, 'https://www.youtube.com/testlesson/updated')
+
+
+
         # path("lessons/", LessonListApiView.as_view(), name="lessons_list"),
         # path("lessons/<int:pk>/", LessonRetrieveApiView.as_view(), name="lessons_retrieve"),
         # path("lessons/create/", LessonCreateApiView.as_view(), name="lessons_create"),
@@ -74,91 +96,15 @@ class MaterialsTestCase(APITestCase):
         # path("lessons/<int:pk>/update/", LessonUpdateApiView.as_view(), name="lessons_update"),
         # path("subscription/<int:pk>/", SubscriptionApiView.as_view(), name="subscription"),
 
-    # def test_lesson_delete(self):
+    def test_lesson_delete(self):
         """Тестирование удаления урока"""
 
-    # def test_subscription(self):
+        response = self.client.delete("lessons/5/", format="json")
+
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+        self.assertEqual(Course.objects.all().count(), 0)
+
+
+        # def test_subscription(self):
         """Тестирование подписки"""
 
-    # def test_lessons_retrieve(self):
-    #     # url = reverse("materials:lessons_detail", args=(self.lessons.pk))
-    #     response = self.client.get("/lessons/<int:pk>/", format="json")
-    #     data = response.json()
-    #     print(response.json())
-    #     self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-# class CourseTestCase(APITestCase):
-#
-#     def setUp(self):
-#         self.user = User.objects.create(email='katifedr@mail.ru')
-#         self.course = Course.objects.create(name='Test Course', owner=self.user)
-#
-#     def test_course_create(self):
-#         url = reverse('lessons_create')
-#         data = {'name': 'New Course', 'owner': self.user.id}
-#         response = self.client.post(url, data)
-#         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-#         self.assertTrue(Course.objects.filter(name='New Course').exists())
-#
-#     def test_course_list(self):
-#         url = reverse('lessons_list')
-#         response = self.client.get(url)
-#         self.assertEqual(response.status_code, status.HTTP_200_OK)
-#
-#
-# class LessonTestCase(APITestCase):
-#     def setUp(self):
-#         self.user = User.objects.create(email='katifedr@mail.ru')
-#         self.course = Course.objects.create(name='Test Course', owner=self.user)
-#         self.lesson = Lesson.objects.create(name='Test Lesson', course=self.course,
-#                                             video_url='https://www.youtube.com/test')
-#
-#     def test_lessons_retrieve(self):
-#         url = reverse('lessons_detail', args=[self.lesson.id])
-#         response = self.client.get(url)
-#         self.assertEqual(response.status_code, status.HTTP_200_OK)
-#         self.assertEqual(response.data['name'], 'Test Lesson')
-#
-#     def test_lessons_create(self):
-#         url = reverse('lessons_create')
-#         data = {
-#             'name': 'New Lesson',
-#             'course': self.course.id,
-#             'video_url': 'https://www.youtube.com/newlesson'
-#         }
-#         response = self.client.post(url, data)
-#         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-#         self.assertTrue(Lesson.objects.filter(name='New Lesson').exists())
-
-#     def test_lesson_update(self):
-#         url = reverse('lessons_update', args=[self.lesson.id])
-#         data = {'name': 'Updated Lesson'}
-#         response = self.client.patch(url, data)
-#         self.assertEqual(response.status_code, status.HTTP_200_OK)
-#         self.lesson.refresh_from_db()
-#         self.assertEqual(self.lesson.name, 'Updated Lesson')
-#
-#     def test_lesson_delete(self):
-#         url = reverse('lessons_delete', args=[self.lesson.id])
-#         response = self.client.delete(url)
-#         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
-#         self.assertFalse(Lesson.objects.filter(id=self.lesson.id).exists())
-#
-# class SubscriptionTestCase(APITestCase):
-#     def setUp(self):
-#         self.user = User.objects.create(email='katifedr@mail.ru')
-#         self.course = Course.objects.create(name='Test Course', owner=self.user)
-#
-#     def test_subscription_create(self):
-#         url = reverse('subscription', args=[self.course.id])
-#         data = {'user': self.user.id, 'course': self.course.id, 'is_subscription': True}
-#         response = self.client.post(url, data)
-#         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-#         self.assertTrue(Subscription.objects.filter(user=self.user, course=self.course).exists())
-#
-#     def test_unsubscription(self):
-#         Subscription.objects.create(user=self.user, course=self.course, is_subscription=True)
-#         url = reverse('subscription', args=[self.course.id])
-#         response = self.client.delete(url)
-#         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
-#         self.assertFalse(Subscription.objects.filter(user=self.user, course=self.course).exists())
